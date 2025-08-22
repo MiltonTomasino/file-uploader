5ae2062a1014b74e1772947db6489c49b2d3a15b
MiltonTomasino <jonathantomasino13@gmail.com>
2025-08-22 14:41:32 -0700

    Allow Logged In Users to Upload Files
    Users can upload files and send them to another folder locally.

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -3,3 +3,31 @@
 module.exports.getHomePage = (req, res) => {
     res.render("upload", { title: "Home Page", user: req.user});
 }
+
+module.exports.uploadFile = async (req, res) => {
+    console.log("Uploaded file: ", req.file);
+    try {
+        if (!req.file) {
+            return res.render("message", {
+                title: "Status01",
+                header: "Upload Failed",
+                message: "No file uploaded."
+            })
+        }
+
+        console.log("File saved to DB:", req.file);
+
+        res.render("message", {
+            title: "Status",
+            header: "Upload Success",
+            message: "File uploaded to database successfully."
+        })
+    } catch (error) {
+        console.error("Error saving file to DB: ", error);
+        res.render("message", {
+            title: "Status02",
+            header: "Upload Error",
+            message: "There was an error saving your file."
+        });
+    }
+}
--- routes/upload.js
+++ routes/upload.js
@@ -1,8 +1,25 @@
 const router = require("express").Router();
 const controller = require("../controllers/uploadController");
 const { isLoggedIn } = require("./authMiddleware");
+const multer = require("multer");
+const path = require("path");
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "/Users/miltontomasino/Desktop/temp_files");
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1E9);
+        cb(null, file.fieldname + "-" + uniqueSuffix + path.extname(file.originalname));
+    }
+})
+
+const upload = multer({ storage: storage });
 
 router.get("/", isLoggedIn, controller.getHomePage);
+
+router.post("/upload-file", upload.single("file"), controller.uploadFile);
+
 router.get("/log-out", (req, res, next) => {
     req.logOut(function (err) {
         if (err) return next(err);
